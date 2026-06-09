a02c26e7f0e7b39a3cb11ba81260ea0314328ad7
Smeet <240905050073s@gmit.edu.in>
2026-06-09 19:18:44 +0530

    creating the all ui of activities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.cardview)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.coordinatorlayout)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
     testImplementation(libs.junit)
